3435d1e4576e30004d2bc6f6376b3ae0e689af59
evisp <evisplaku@gmail.com>
2022-09-10 09:39:17 -0700

    uupdate task 3

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -8,27 +8,28 @@ Records all tasks from all employees
 Format must be: { "USER_ID": [ {"username": "USERNAME", "task": "TASK_TITLE", "completed": TASK_COMPLETED_STATUS}, {"username": "USERNAME", "task": "TASK_TITLE", "completed": TASK_COMPLETED_STATUS}, ... ], "USER_ID": [ {"username": "USERNAME", "task": "TASK_TITLE", "completed": TASK_COMPLETED_STATUS}, {"username": "USERNAME", "task": "TASK_TITLE", "completed": TASK_COMPLETED_STATUS}, ... ]}
 File name must be: todo_all_employees.json
 """
+
 import json
 import requests
-
+from sys import argv
 
 if __name__ == "__main__":
-    jsonFile = 'todo_all_employees.json'
-    url_user = 'https://jsonplaceholder.typicode.com/users'
-    url_todo = 'https://jsonplaceholder.typicode.com/todos'
-    user_req = requests.get(url_user).json()
-    todo_req = requests.get(url_todo).json()
-    json_format = {}
-    for all_users in user_req:
-        info_user_list = []
-        for all_tasks in todo_req:
-            if all_users['id'] == all_tasks['userId']:
-                user_infor_dict = {
-                    "username": all_users['username'],
-                    "task": all_tasks['title'],
-                    "completed": all_tasks['completed'],
-                }
-                info_user_list.append(user_infor_dict)
-        json_format[all_users['id']] = info_user_list
-    with open(jsonFile, "w") as file:
-        json.dump(json_format, file)
+    url_users = "https://jsonplaceholder.typicode.com/users"
+    url_todos = "https://jsonplaceholder.typicode.com/todos"
+    user_request = requests.get(url_users).json()
+    all_requests = requests.get(url_todos).json()
+
+    emp_data_json = {}
+    for users in user_request:
+        emp_data = []
+        for request in all_requests:
+            if users['id'] == request['userId']:
+                emp_data.append({
+                    "username" : users['username'],
+                    "task" : request['title'],
+                    "completed": request['completed']
+                })
+        emp_data_json[users['id']] = emp_data
+
+    with open("todo_all_employees.json", "w") as file:
+        json.dump(emp_data_json, file)
